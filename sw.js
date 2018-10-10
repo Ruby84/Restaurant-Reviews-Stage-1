55ef938e6ef96b8482af463900b2f1f8b3cf356f
Roudabeh <rjamshid@my.bridgeport.edu>
2018-10-10 12:28:40 -0400

    Edited version

--- sw.js
+++ sw.js
@@ -58,3 +58,15 @@ self.addEventListener('fetch', event => {
     caches.match(request).then(response => response || fetch(request)),
   );
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
